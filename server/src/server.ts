42a29ab6394cdc30c53974e9a2d40067191e8c22
Armel PEIRANO <armel.peirano@epitech.eu>
2026-03-18 11:55:50 +0100

    ADD: typescript server base

---
+++ server/src/server.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from "express";
+import sqlite3 from "sqlite3";
+
+const app = express();
+app.use(express.json());
+
+// Owner type
+type Owner = {
+    id: number;
+    name: string;
+    lastname: string;
+    phone_number: number;
+    mail: string;
+};
+
+enum Book_condition {};
+
+// Book type
+type Book = {
+  id: string;
+  title: string;
+  author: string;
+  owner: Owner;
+  available: boolean;
+  condition: Book_condition;
+  remaining_days: number;
+};
